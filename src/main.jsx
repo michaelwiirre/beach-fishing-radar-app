d5567b0b996c87932747993425b6275ad1a44869
michaelwiirre <michaelwiirre@outlook.com>
2026-09-08 08:05:46 -0400

    Create main.jsx

---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import BeachFishingRadar from "./App.jsx";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <BeachFishingRadar />
+  </React.StrictMode>
+);
